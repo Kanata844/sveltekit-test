b49cf8f50b55f1dc7992dd1235e59b790b9e96a9
Kanata844 <ta3wata@gmail.com>
2025-12-13 19:17:34 +0900

    最初のコミット

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="/page2">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ src/routes/page2/page3/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let n = $state(0)
+</script>
+
+<h1>これはさらに別のページです</h1>
+<p><a href="/">元のページに戻る</a></p>
+<button onclick={()=>{n++}}>{n}</button>
